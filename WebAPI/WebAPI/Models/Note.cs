6fac43f653c50943e3ad6c1ebf3a25066143a289
Habiba El-Sayed <habiba192213@fci.bu.edu.eg>
2024-05-11 18:24:18 +0300

    Adding note model

---
+++ WebAPI/WebAPI/Models/Note.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI.Models
+{
+    public class Note
+    {
+        [Key]
+        public int ID { get; set; }
+        public Guid ExposedID { get; set; }
+        public string Relation { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastModified { get; set;}
+
+        // TODO: change this to User instead of ApplicationUser in the future
+        public ApplicationUser CreatedBy { get; set; }
+    }
+}
